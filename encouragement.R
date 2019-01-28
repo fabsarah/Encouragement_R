a6024d56d11a1a2e8f6867df87ad786c322cb887
Sarah Faber <31863074+fabsarah@users.noreply.github.com>
2019-01-28 18:16:34 -0500

    Add files via upload

---
+++ encouragement.R
@@ -0,0 +1,26 @@
+##A robot that lives in your R Console and dispenses encouragement
+encouragement <- function(){
+  a=(1:10)
+  a<-sample(a,1)
+if (a==1){
+print('You can do it!')
+}else if (a==2){
+print('I believe in you!')
+}else if (a==3){
+print('You are doing great!')
+}else if (a==4){
+  print('You can do the thing!')
+}else if (a==5){
+  print('You got this!')
+}else if (a==6){
+  print('Who has infinite thumbs and thinks you are awesome? Me!')
+}else if (a==7){
+  print('If I could make tea, I would make you a cup of tea!')
+}else if (a==8){
+  print('You are a magical unicorn!')
+}else if (a==9){
+  print('You are an amazing scientist!')
+}else if (a==10){
+  print('Here is a Nobel prize!')
+}
+}
